3f87a47f67efd0521dcffee805cb9f86c87b86c2
abuyusufrony <abuyusufrony@gmail.com>
2024-12-28 11:27:40 +0600

    high-function

--- max.js
+++ max.js
@@ -1,8 +1,18 @@
-const rony=56;
-const salman=70;
-if (rony>salman){
-    console.log('rony get the price')
+// const rony=56;
+// const salman=70;
+// if (rony>salman){
+//     console.log('rony get the price')
+// }
+// else{
+//     console.log('salman will get the price',salman)
+// }
+function max(rony,bony){
+if(rony>bony){
+    return rony;
 }
 else{
-    console.log('salman will get the price',salman)
+    return bony;
 }
+}
+const high=max(97,95);
+console.log('the highest number are',high)
