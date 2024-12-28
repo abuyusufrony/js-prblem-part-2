a9acdd91343377b3b3bf69e2a9fd19969bdc6fd2
abuyusufrony <abuyusufrony@gmail.com>
2024-12-28 11:43:17 +0600

    three-function

--- max.js
+++ max.js
@@ -6,13 +6,37 @@
 // else{
 //     console.log('salman will get the price',salman)
 // }
-function max(rony,bony){
-if(rony>bony){
-    return rony;
+// function max(rony,bony){
+// if(rony>bony){
+//     return rony;
+// }
+// else{
+//     return bony;
+// }
+// }
+// const high=max(97,95);
+// console.log('the highest number are',high)
+
+
+
+// use for 3 person which is bigger than
+function threeperson( sony,bony,rony){
+    if(sony>bony&& sony>rony){
+        console.log('sony is the boss')
+        return sony;
+    }
+    else if(bony>rony&&bony>sony){
+console.log('bony is the boss')
+return bony;
+    
 }
-else{
-    return bony;
+    
+    else{
+        console.log('rony is the boss')
+        return rony;
+    }
+    
+
 }
-}
-const high=max(97,95);
-console.log('the highest number are',high)
+const max=threeperson(20,32,30);
+console.log(max)
