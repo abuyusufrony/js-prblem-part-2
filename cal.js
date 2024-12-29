e6cca3bc46fcaf391e7810bd119275e17708b68a
abuyusufrony <abuyusufrony@gmail.com>
2024-12-29 18:20:36 +0600

    task-1

--- cal.js
+++ cal.js
@@ -1,65 +1,41 @@
-// function add(num1,num2){
-//     const sum=num1+num2;
-//     return sum;
-// }
-// function subtrack(num1,num2){
-//     const result =num1-num2;
-//     return result;
-// }
-// function multiple(num1,num2){
-//     const result=num1*num2;
-//     return result;
-// }
-// function divide(num1, num2){
-//     const result=num1/num2;
-//     return result;
-// }
-// function calculator(a,b,operator){
-//     if (operator===add){
-//         const result =add(a,b);
-//         return result;
-//     }
-//     else if(operator===subtrack){
-//         const result=subtrack(a,b);
-//         return result;
-
-//     }
-//     else if(operator=divide){
-//         const result=divide(a,b);
-//         return result;
-//     }
-//     else if(operator=multiple){
-//         const result= multiple(a,b)
-//         return result;
-//     }
-//     else {
-//         return ;
-//     }
-// }
-// const givenValue=calculator(25,5,'add')
-// console.log(givenValue)
-
 function add(num1,num2){
-     const sum=num1+num2;
+    const sum=num1+num2;
     return sum;
 }
-function sub(num1,num2){
-    const sub=num1-num2;
-    return sub;
+function subtrack(num1,num2){
+    const result =num1-num2;
+    return result;
 }
-function calculator(a,b,operation){
-    if(operation==='add'){
-        const result=add(a,b)
+function multiple(num1,num2){
+    const result=num1*num2;
+    return result;
+}
+function divide(num1, num2){
+    const result=num1/num2;
+    return result;
+}
+function calculator(a,b,operator){
+    if (operator==='add'){
+        const result =add(a,b);
         return result;
     }
-    else if(operation==='sub'){
-        const result=sub(a,b)
+    else if(operator==='subtrack'){
+        const result=subtrack(a,b);
+        return result;
+
+    }
+    else if(operator='divide'){
+        const result=divide(a,b);
         return result;
     }
-   else{
-    return "only,'add','multiple'     ";
-   }
-    
+    else if(operator='multiple'){
+        const result=(a,b);
+        return result;
+    }
+    else {
+        return ;
+    }
 }
-const finalOut=calculator(10,5,'sub')
-console.log(finalOut)
+const givenValue=calculator(25,5,'divide')
+console.log(givenValue)
+
---
+++ task-1.js
@@ -0,0 +1,16 @@
+function lowestValue(numbers){
+    let short=numbers[0]
+for(const num of numbers){
+    if(short>num){
+       short=num;
+       
+    }
+    
+}
+return short;
+
+
+}
+const  heights2 = [167, 190, 120, 165, 137];
+const small=lowestValue(heights2)
+console.log('the shortes number are',small)
